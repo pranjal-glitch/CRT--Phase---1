f1e8a032db408923e9c602ddac73dc4dea1b92ee
pranjal-glitch <pranjal29240705@gmail.com>
2026-05-04 23:30:51 +0530

    Add pyramid pattern printing program in C++
    This program prints a centered pyramid pattern using asterisks based on user input for the number of rows.

---
+++ Day-4/pyramid.cpp
@@ -0,0 +1,36 @@
+/* Description
+
+This program prints a centered pyramid pattern using *.
+The number of rows is taken as input from the user.
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+
+    // Take number of rows as input
+    cout << "Enter number of rows: ";
+    cin >> n;
+
+    // Loop for each row
+    for(int i = 1; i <= n; i++) {
+
+        // Print spaces (for alignment)
+        for(int j = 1; j <= n - i; j++) {
+            cout << " ";
+        }
+
+        // Print stars
+        // Formula: 2*i - 1 → gives odd numbers (1, 3, 5, ...)
+        for(int j = 1; j <= 2*i - 1; j++) {
+            cout << "*";
+        }
+
+        // Move to next line after each row
+        cout << endl;
+    }
+
+    return 0;
+}
