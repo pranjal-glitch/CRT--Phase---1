3a415a8349c55c3df8dfca586813d211ecd35741
pranjal-glitch <pranjal29240705@gmail.com>
2026-05-06 15:05:31 +0530

    Add sum function to calculate the sum of two integers

---
+++ Day - 5/functions/sum.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+void add_int(){
+   int a,b;
+   cin >> a >> b;
+   int sum = a + b;
+    cout << sum << endl;
+}
+
+int main(){
+
+    add_int();
+    return 0;
+}
