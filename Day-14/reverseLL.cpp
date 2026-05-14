37a13aa8e62463a39c49108c9569fc3f68c42518
pranjal-glitch <pranjal29240705@gmail.com>
2026-05-14 13:22:16 +0530

    Implement function to reverse a linked list

---
+++ Day-14/reverseLL.cpp
@@ -0,0 +1,20 @@
+
+Node* reverseLinkedList(Node *head)
+{
+    // Write your code here
+    if(head == NULL || head -> next == NULL){
+        return head;
+    }
+    Node* prev = NULL;
+    Node* curr = head;
+    Node* forward = NULL;
+    
+    while(curr != NULL){
+        forward = curr -> next;
+        curr -> next = prev;
+
+        prev = curr;
+        curr = forward;
+    }
+    return prev;
+}
